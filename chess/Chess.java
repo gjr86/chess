d25888d92b7084d3ae99c540e9266e8f9ca86492
Jon <78041305+xxjonmax@users.noreply.github.com>
2025-10-01 18:53:09 -0400

    Update Chess.java

--- chess/Chess.java
+++ chess/Chess.java
@@ -6,6 +6,10 @@ public class Chess {
 
     enum Player { white, black }
 
+    // Static fields because play() is static
+    private static Board board;
+    private static Player currentTurn;
+
     /**
      * Plays the next move for whichever player has the turn.
      *
@@ -16,12 +20,76 @@ public class Chess {
      *         the contents of the returned ReturnPlay instance.
      */
     public static ReturnPlay play(String move) {
+      move = move.trim();
 
-        /* FILL IN THIS METHOD */
+        // Handle resign
+        if (move.equals("resign")) {
+            ReturnPlay.Message msg = (currentTurn == Player.white)
+                    ? ReturnPlay.Message.RESIGN_BLACK_WINS
+                    : ReturnPlay.Message.RESIGN_WHITE_WINS;
+            return new ReturnPlay(board.toReturnPieces(), msg);
+        }
 
-        /* FOLLOWING LINE IS A PLACEHOLDER TO MAKE COMPILER HAPPY */
-        /* WHEN YOU FILL IN THIS METHOD, YOU NEED TO RETURN A ReturnPlay OBJECT */
-        return null;
+        // Handle draw offer (simplified)
+        if (move.endsWith("draw?")) {
+            // Accepting draw will be handled by opponent move
+            // For now just strip it off
+            move = move.replace(" draw?", "");
+        }
+
+        // Parse move string: "a2 a3"
+        String[] parts = move.split(" ");
+        if (parts.length < 2) {
+            return new ReturnPlay(board.toReturnPieces(), ReturnPlay.Message.ILLEGAL_MOVE);
+        }
+
+        String from = parts[0];
+        String to = parts[1];
+
+        char fromFile = from.charAt(0);
+        int fromRank = Character.getNumericValue(from.charAt(1));
+        char toFile = to.charAt(0);
+        int toRank = Character.getNumericValue(to.charAt(1));
+
+        // Find piece
+        Piece piece = board.getPieceAt(fromFile, fromRank);
+        if (piece == null || piece.getColor() != currentTurn) {
+            return new ReturnPlay(board.toReturnPieces(), ReturnPlay.Message.ILLEGAL_MOVE);
+        }
+
+        // Check legality
+        if (!piece.isLegalMove(toFile, toRank, board)) {
+            return new ReturnPlay(board.toReturnPieces(), ReturnPlay.Message.ILLEGAL_MOVE);
+        }
+
+        // Tentatively move on copy of board
+        Board copy = board.clone();
+        copy.movePiece(fromFile, fromRank, toFile, toRank);
+
+        // If move leaves your own king in check → illegal
+        if (copy.isInCheck(currentTurn)) {
+            return new ReturnPlay(board.toReturnPieces(), ReturnPlay.Message.ILLEGAL_MOVE);
+        }
+
+        // Commit move
+        board.movePiece(fromFile, fromRank, toFile, toRank);
+
+        // Check opponent state
+        Player opponent = (currentTurn == Player.white) ? Player.black : Player.white;
+        ReturnPlay.Message msg = null;
+
+        if (board.isCheckmate(opponent)) {
+            msg = (opponent == Player.white)
+                    ? ReturnPlay.Message.CHECKMATE_BLACK_WINS
+                    : ReturnPlay.Message.CHECKMATE_WHITE_WINS;
+        } else if (board.isInCheck(opponent)) {
+            msg = ReturnPlay.Message.CHECK;
+        }
+
+        // Switch turn
+        currentTurn = opponent;
+
+        return new ReturnPlay(board.toReturnPieces(), msg);
     }
 
 
@@ -29,6 +97,9 @@ public class Chess {
      * This method should reset the game, and start from scratch.
      */
     public static void start() {
-        /* FILL IN THIS METHOD */
+        board = new Board();
+        board.setup();
+        currentTurn = Player.white;
     }
+
 }
